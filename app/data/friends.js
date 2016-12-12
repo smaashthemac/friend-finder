b0a734181cdc7f6f88f5ddfd1f435b695abf2c0c
Christ <smaashthemac@gmail.com>
2016-12-12 09:08:19 -0600

    integers, yay

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,5 @@
-var friendArray = [
-	{
-    name: "Christina",
-    image: "A black and white picture of me in a tree",
-    scores: ["2", "3", "5", "4", "3", "1", "1", "2", "4", "5"]
-    }
-];
-
-// var friendScores = friendArray.scores.split(',').map(function(item) {
-//     return parseInt(item, 10);
-// });
-
-console.log(friendArray);
-console.log(friendArray.scores[0]);
-
+var newFriend = [];
 
 // export the array
-module.exports = friendArray;
+module.exports = newFriend;
+
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,27 +1,43 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+var Friends = require('../data/friends.js');
 
-var friendsData = require("../data/friends.js");
+module.exports = function(app){
+
+	// API GET Requests
+	app.get('/api/friends', function(req, res){
+		res.json(Friends);
+	});
 
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+	// user submits a form and it submits data to the server.
+	app.post('/api/friends', function(req, res){
 
-module.exports = function(app) {
+	// setting a variable for the user's response
+    var userData = req.body;
+    	// the total difference starts off at 0 
+		var totalDifference = 0;
+		// all differences is an empty array into which the scores of each potential friend the user is being compared to will go
+		var allDifferences = []; 
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
-    console.log(friendsData);
-  });
+	//loop through all of the stored friends 
+	for (var i=0; i<(friends.length-1); i++){
 
-  app.post("/api/friends", function(req, res) {
-    friendsData.push(req.body);
-    res.json(friendsData);
-    console.log(friendsData);
-  });
+		//loop through all of question values and sum total their subtracted absolute values
+		for (var j=0; j<10; j++){
+			// this adds the numerical answers of each friend to the total difference; then uses the absolute value to determine the difference between the two (absolute value is used so that 5-3 and 3-5 both equal 2)
+			totalDifference += Math.abs(friends[i].scores[j] - newFriend.scores[j]);
+		}
 
-};
+		// each total difference, for each potential friend, is pushed into the allDifferences array 
+		allDifferences.push(totalDifference);
+		// total difference is reset back to zero
+		totalDifference = 0;
+	}
+
+	//best match will give the smallest values 
+	var bestMatch = friends[allDifferences.indexOf(Math.min.apply(null, allDifferences))];
+
+	res.send(bestMatch);
+	console.log(bestMatch);
+
+	});
+}
